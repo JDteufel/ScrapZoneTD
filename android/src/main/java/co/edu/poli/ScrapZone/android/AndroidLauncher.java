e1016ae02be200e75fa8be631a42e159e73356c2
JDteufel <jgomezcardenas30@gmail.com>
2025-05-07 17:18:15 -0500

    Primer commit: estructura base del proyecto con GDX-Liftoff

---
+++ android/src/main/java/co/edu/poli/ScrapZone/android/AndroidLauncher.java
@@ -0,0 +1,20 @@
+package co.edu.poli.ScrapZone.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.github.czyzby.autumn.android.scanner.AndroidClassScanner;
+import com.github.czyzby.autumn.mvc.application.AutumnApplication;
+import co.edu.poli.ScrapZone.Main;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        configuration.useImmersiveMode = true; // Recommended, but not required.
+        initialize(new AutumnApplication(new AndroidClassScanner(), Main.class), configuration);
+    }
+}
---
+++ core/src/main/java/co/edu/poli/ScrapZone/service/controls/ControlListener.java
@@ -0,0 +1,7 @@
+package co.edu.poli.ScrapZone.service.controls;
+
+/** Listens to game events. */
+public interface ControlListener {
+    /** Invoked when controller detects jump input event. */
+    void jump();
+}
